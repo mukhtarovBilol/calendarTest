ebb4151011c70ab0ca57284ebf466f77f44181f0
Bilol <bilol@gmail.com>
2024-09-29 15:23:14 +0500

    f

--- sc.js
+++ sc.js
@@ -5,4 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const today = new Date();
     const formattedDate = today.toISOString().split('T')[0];
     dateInput.setAttribute('min', formattedDate);
+    
+    const form = document.getElementById('dateForm');
+
+    form.addEventListener('submit', function(e) {
+        const selectedDate = new Date(dateInput.value);
+        today.setHours(0, 0, 0, 0); // Устанавливаем время на 00:00:00
+
+        if (selectedDate < today) {
+            e.preventDefault(); // Отменяем отправку формы
+            alert('Выберите дату сегодня или в будущем.');
+            dateInput.value = ''; // Очищаем поле ввода
+        }
+    });
 });
